9f22d6837af511c20e1147e25f2825d09d076f22
RandyNoodles <josh.rice193@gmail.com>
2025-04-11 19:40:13 -0400

    Finished Like buttons, adding first modal.

--- src/posts/views.py
+++ src/posts/views.py
@@ -14,7 +14,7 @@ def load_post_data_view(request, num_posts):
     upper = num_posts
     lower = upper - visible
     size = Post.objects.all().count()
-
+    qs = Post.objects.all()
     data = []
     for obj in qs:
         item = {
@@ -27,6 +27,20 @@ def load_post_data_view(request, num_posts):
         }
         data.append(item)
     return JsonResponse({'data':data[lower:upper], 'size': size})
+
+def like_unlike_post(request):
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        pk = request.POST.get('pk')
+        obj = Post.objects.get(pk=pk)
+        if request.user in obj.liked.all():
+            liked = False
+            obj.liked.remove(request.user)
+        else:
+            liked = True
+            obj.liked.add(request.user)
+        return JsonResponse({'liked': liked, 'count': obj.like_count})
+
+
 
 def hello_world_view(request):
     return JsonResponse({'text': 'hello worldddd'})
