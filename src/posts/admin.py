547c73829534c60125cee8e075756d04c1c7ff7a
RandyNoodles <josh.rice193@gmail.com>
2025-04-11 18:37:41 -0400

    Fixed "like" bug in admin. Done Like Button part 1

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Post
 
-# Register your models here.
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author']
+    filter_horizontal = ['liked']  # This enables proper many-to-many editing
